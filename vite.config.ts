621332cbf4425b693abbd7a547349f2c2088dca8
lzray-universe <87802707+lzray-universe@users.noreply.github.com>
2025-09-24 08:19:23 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES ? '/wordmorph/' : '/',
+  base: '/',            // 自定义域/子域名用根路径
   build: { sourcemap: false }
 })
